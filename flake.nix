816a77d9b08799b9406e0ea156bca9f79f5d0eef
Uzair Aftab <uzaaft@outlook.com>
2025-04-19 19:58:32 +0200

    chore: use nixpkgs, not nixos

--- flake.nix
+++ flake.nix
@@ -2,7 +2,8 @@
   description = "An empty project that uses Zig.";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-24.11";
+    nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
+
     flake-utils.url = "github:numtide/flake-utils";
     zig.url = "github:mitchellh/zig-overlay";
 
